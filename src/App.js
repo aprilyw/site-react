19e5c5dd00340d95c79a8a5d825876be1ad11a45
April <aprilyanli38@gmail.com>
2021-04-18 22:17:02 -0500

    model

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Model from "./grandeDisco"
+import {Canvas} from "@react-three/fiber"
+import { Suspense } from 'react'
+import { OrbitControls, Html, useProgress } from "@react-three/drei"
+
+
+function Loader() {
+  const { progress } = useProgress()
+  return <Html center color="black">{progress} % loaded</Html>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Canvas style={{height: 1000,}}>
+      <ambientLight intensity={1} />
+      <Suspense fallback={<Loader />}>
+        <Model />
+      </Suspense>
+      <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={true} enablePan={true} autoRotate={true}/>
+
+    </Canvas>
     </div>
   );
 }
---
+++ src/grandeDisco.js
@@ -0,0 +1,22 @@
+import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+import path from "./models/grandeDisco.glb"
+
+function Model(props) {
+  const group = useRef()
+  const { nodes, materials } = useGLTF(path)
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Mesh_0.geometry}
+        material={materials.Material_0}
+        position={[0, 0.7, 0]}
+        rotation={[-0.2, 0, 0]}
+      />
+    </group>
+  )
+}
+
+export default Model;
